73467894e532e702397b22e0938e90aaa36e3b44
Luis Valdivia <luisvaldivia@Luiss-MacBook-Air.local>
2025-04-11 16:27:10 -0400

    fast fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/',
   plugins: [vue()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html', // make sure this is correct
+    },
+  }
 })
